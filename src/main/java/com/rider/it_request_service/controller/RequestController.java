52bbdf3933d9efbf675e400930383c085983ac8f
Boy2002 <wanchai.w16@gmail.com>
2025-02-16 19:09:58 +0700

    fix (add request can not request "files")

--- src/main/java/com/rider/it_request_service/controller/RequestController.java
+++ src/main/java/com/rider/it_request_service/controller/RequestController.java
@@ -118,7 +118,7 @@ public class RequestController {
     public ResponseEntity<Object> RecordRequestForm(
             @AuthenticationPrincipal CustomUserDetails user,
             @RequestPart(value = "request", required = true) String jsonObj,
-            @RequestPart("files") MultipartFile[] files,
+            @RequestPart(value = "files", required = false) MultipartFile[] files,
             BindingResult bindingResult) {
 
         try {
